7a9a6e8c15e9270d7e4953aa3ebaa5dca1d2e110
Alex <alex.chen@goldenf.co>
2022-07-12 11:49:52 +0800

    Initial Commit

---
+++ Thaigi-HV/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Thaigi-HV
+//
+//  Created by Alex on 2022/7/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
